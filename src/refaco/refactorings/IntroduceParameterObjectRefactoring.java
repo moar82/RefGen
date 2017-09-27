6158cd819fb825aa61c1ae0048b3eff306fe580a
moar82 <rodrigomorales2@acm.org>
2017-09-27 17:46:23 -0400

    fix IPO to refactor all methods in the list, and not just the first method.  Also improve the generation of the new name using double instead of int to reduce name collisions

--- src/refaco/refactorings/IntroduceParameterObjectRefactoring.java
+++ src/refaco/refactorings/IntroduceParameterObjectRefactoring.java
@@ -18,6 +18,8 @@ import org.eclipse.jdt.core.IPackageFragmentRoot;
 import org.eclipse.jdt.core.IType;
 import org.eclipse.jdt.core.JavaCore;
 import org.eclipse.jdt.core.Signature;
+import org.eclipse.jdt.core.dom.AST;
+import org.eclipse.jdt.core.dom.ASTParser;
 import org.eclipse.jdt.core.refactoring.IJavaRefactorings;
 import org.eclipse.jdt.core.refactoring.descriptors.IntroduceParameterObjectDescriptor;
 import org.eclipse.jdt.core.refactoring.descriptors.MoveMethodDescriptor;
@@ -50,105 +52,130 @@ public class IntroduceParameterObjectRefactoring extends refaco.refactorings.Ref
 	
 	private static String createNewClass(String sourceClass) {
 		final java.util.Random rand = new java.util.Random();
-        String newClassName = ( "Class"+ rand.nextInt(15)+sourceClass );
+        String newClassName = ("Clazz" + rand.nextDouble()+sourceClass).replace('.', '0');
         return newClassName;
     }
 
 	public void apply() throws RefactoringException  {
-		
-		
+
+
 		// Get the package and class name
 		String packageAndClass = getRefactoringData().getClassSource();
 		int index = packageAndClass.lastIndexOf('.');
-		String packageName = packageAndClass.substring(0, index);
-		String className = packageAndClass.substring(index + 1, packageAndClass.length());
-		
-		// Get the method and parameters
-		String methodAndParameters = getRefactoringData().getMethodTarget();
-		String methodName = methodAndParameters.substring(0, methodAndParameters.indexOf('(')).replaceAll("\\s","");
-		if(methodName.equals("<init>")){
-			methodName = className;
+		String packageName;
+		String className;
+		if (index==-1){
+			className = packageAndClass;
+			packageName = IPackageFragment.DEFAULT_PACKAGE_NAME;
 		}
-		String[] parameters = methodAndParameters.substring(methodAndParameters.indexOf('(') +1,methodAndParameters.indexOf(')')).split(",");
-		String[] parametersTypes = new String[parameters.length];
-		for(int i=0; i < parameters.length; i++){
-			// remove the white spaces
-			parametersTypes[i] = Signature.getSimpleName(parameters[i].replaceAll("\\s",""));
-			parametersTypes[i] = Signature.createTypeSignature(parametersTypes[i], false);
+		else{
+			packageName = packageAndClass.substring(0, index);
+			className = packageAndClass.substring(index + 1, packageAndClass.length());	
 		}
-		
-		// Get the IProject from the projectName
-		IWorkspace workspace = ResourcesPlugin.getWorkspace();
-		IWorkspaceRoot root = workspace.getRoot();
-		IProject project = root.getProject(getProjectName());
-		
-		try {
-			if (project.isNatureEnabled("org.eclipse.jdt.core.javanature")) {
 
-				// Get the IMethod
-				IJavaProject javaProject = JavaCore.create(project);
-				IPackageFragmentRoot rootpackage = javaProject.getPackageFragmentRoot(project.getFolder(CodeSmellHandler.javasrc));
-				IPackageFragment classPackage = rootpackage.getPackageFragment(packageName);
-				ICompilationUnit classCU = classPackage.getCompilationUnit(className + ".java");
-				IType type = classCU.getType(className);
-				IMethod method = type.getMethod(methodName, parametersTypes);
+		// Get the methods and parameters
+		String[] methodsAndParameters = getRefactoringData().getMethodTarget().split(";");
 
-				// check if the method has been found
-				// if not search the method by name and number of parameters
-				if(method==null || !method.exists()){
-					IMethod[] methods = type.getMethods();
-					method = null;
-					int i = 0;
-					while(method == null && i < methods.length){
-						IMethod me = methods[i];
-						if (me.getElementName().equals(methodName) && me.getNumberOfParameters() == parametersTypes.length) {
-							method = me;
-						}
-						i++;
-					}
-				}
+		for (String methodAndParameters: methodsAndParameters){
 
-				// check if the method has been found
-				if (method != null && method.exists()) {
-					// Initialize the refactoring descriptor
-					RefactoringContribution contribution = RefactoringCore
-							.getRefactoringContribution(IJavaRefactorings.INTRODUCE_PARAMETER_OBJECT);
-					IntroduceParameterObjectDescriptor descriptor = (IntroduceParameterObjectDescriptor) contribution
-							.createDescriptor();
-					String name = "New"+createNewClass(className);
-					descriptor.setClassName(name);
-					descriptor.setParameterName("Object");
-					descriptor.setPackageName(classPackage.getElementName());
-					descriptor.setProject(javaProject.getElementName());
-					descriptor.setMethod(method);
-					// Create the classes needed for apply the refactoring
-					IntroduceParameterObjectProcessor processor = new IntroduceParameterObjectProcessor(descriptor);
-					Refactoring refactoring = new ProcessorBasedRefactoring(processor);
-					IntroduceParameterObjectWizard wizard = new IntroduceParameterObjectWizard(processor, refactoring);
-					RefactoringWizardOpenOperation op = new RefactoringWizardOpenOperation(wizard);
-					IProgressMonitor monitor = new NullProgressMonitor();
-					try {
-						refactoring.checkInitialConditions(monitor);
-						RefactoringStatus status = new RefactoringStatus();
-						refactoring.checkFinalConditions(monitor);
-						Change change = refactoring.createChange(monitor);
-						change.initializeValidationData(monitor);
-						change.perform(monitor);
-					} catch (OperationCanceledException  | NullPointerException e) {
-						//e.printStackTrace();
-						saved.saving("IntroduceParameterObjectRefactoring", e.toString()+"\n"
-								+ "src: "+packageAndClass+" method name:" + methodName );
-					}
-
-				} else{saved.saving("IntroduceParameterObjectRefactoring", "cannot be applied cause the argument is null "+
-						"src: "+packageAndClass+" method name:" + methodName );
-				}
-			} else {
-				throw new RefactoringException("Java Nature disabled");
+			//String methodAndParameters = getRefactoringData().getMethodTarget();
+			String methodName = methodAndParameters.substring(0, methodAndParameters.indexOf('(')).replaceAll("\\s","");
+			if(methodName.equals("<init>")){
+				methodName = className;
 			}
-		} catch (CoreException e1 ) {
-			e1.printStackTrace();
-			throw new RefactoringException(e1.getMessage());
-		} 
+			String[] parameters = methodAndParameters.substring(methodAndParameters.indexOf('(') +1,methodAndParameters.indexOf(')')).split(",");
+			String[] parametersTypes = new String[parameters.length];
+			for(int i=0; i < parameters.length; i++){
+				// remove the white spaces
+				parametersTypes[i] = Signature.getSimpleName(parameters[i].replaceAll("\\s",""));
+				parametersTypes[i] = Signature.createTypeSignature(parametersTypes[i], false);
+			}
+
+			// Get the IProject from the projectName
+			IWorkspace workspace = ResourcesPlugin.getWorkspace();
+			IWorkspaceRoot root = workspace.getRoot();
+			IProject project = root.getProject(getProjectName());
+
+			try {
+				if (project.isNatureEnabled("org.eclipse.jdt.core.javanature")) {
+
+					// Get the IMethod
+					IJavaProject javaProject = JavaCore.create(project);
+					IPackageFragmentRoot rootpackage = javaProject.getPackageFragmentRoot(project.getFolder(CodeSmellHandler.javasrc));
+					IPackageFragment classPackage = rootpackage.getPackageFragment(packageName);
+					ICompilationUnit classCU = classPackage.getCompilationUnit(className + ".java");
+					IType type = classCU.getType(className);
+					IMethod method = type.getMethod(methodName, parametersTypes);
+
+					// check if the method has been found
+					// if not search the method by name and number of parameters
+					if(method==null || !method.exists()){
+						IMethod[] methods = type.getMethods();
+						method = null;
+						int i = 0;
+						while(method == null && i < methods.length){
+							IMethod me = methods[i];
+							if (me.getElementName().equals(methodName) && me.getNumberOfParameters() == parametersTypes.length) {
+								method = me;
+							}
+							i++;
+						}
+					}
+
+					// check if the method has been found
+					if (method != null && method.exists()) {
+						// Initialize the refactoring descriptor
+						RefactoringContribution contribution = RefactoringCore
+								.getRefactoringContribution(IJavaRefactorings.INTRODUCE_PARAMETER_OBJECT);
+						IntroduceParameterObjectDescriptor descriptor = (IntroduceParameterObjectDescriptor) contribution
+								.createDescriptor();
+						String name = "New"+createNewClass(className);
+/*						while(true) {
+							name = "New"+createNewClass(className);
+							ICompilationUnit targetClassCU = classPackage.getCompilationUnit(name + ".java");
+							ASTParser parserClassChanged = ASTParser.newParser(AST.JLS8);
+							parserClassChanged.setSource(targetClassCU);
+							parserClassChanged.setKind(ASTParser.K_COMPILATION_UNIT);
+							parserClassChanged.setResolveBindings(true);
+							if (typeTarget==null)
+								break;
+						}
+*/						descriptor.setClassName(name);
+						descriptor.setParameterName("Object");
+						descriptor.setPackageName(classPackage.getElementName());
+						descriptor.setProject(javaProject.getElementName());
+						descriptor.setMethod(method);
+						descriptor.setGetters(true);
+						descriptor.setSetters(true);
+						// Create the classes needed for apply the refactoring
+						IntroduceParameterObjectProcessor processor = new IntroduceParameterObjectProcessor(descriptor);
+						Refactoring refactoring = new ProcessorBasedRefactoring(processor);
+						IntroduceParameterObjectWizard wizard = new IntroduceParameterObjectWizard(processor, refactoring);
+						RefactoringWizardOpenOperation op = new RefactoringWizardOpenOperation(wizard);
+						IProgressMonitor monitor = new NullProgressMonitor();
+						try {
+							refactoring.checkInitialConditions(monitor);
+							RefactoringStatus status = new RefactoringStatus();
+							refactoring.checkFinalConditions(monitor);
+							Change change = refactoring.createChange(monitor);
+							change.initializeValidationData(monitor);
+							change.perform(monitor);
+						} catch (OperationCanceledException  | NullPointerException e) {
+							//e.printStackTrace();
+							saved.saving("IntroduceParameterObjectRefactoring", e.toString()+"\n"
+									+ "src: "+packageAndClass+" method name:" + methodName );
+						}
+
+					} else{saved.saving("IntroduceParameterObjectRefactoring", "cannot be applied cause the argument is null "+
+							"src: "+packageAndClass+" method name:" + methodName );
+					}
+				} else {
+					throw new RefactoringException("Java Nature disabled");
+				}
+			} catch (CoreException e1 ) {
+				e1.printStackTrace();
+				throw new RefactoringException(e1.getMessage());
+			} 
+		}
 	}
 }
